fce5e2122d50bd02740e78ae2fc9f8dd7327b1a4
Wil Parsons <entrocraft@gmail.com>
2024-02-26 15:06:52 -0800

    Updating algorithm to prevent short-range linear collisions.

--- stormdrop.c
+++ stormdrop.c
@@ -1,10 +1,11 @@
 #include "stormdrop.h"
 
 uint32_t stormdrop(uint32_t entropy) {
+  entropy += 11;
   entropy += entropy << 3;
-  entropy ^= ((entropy ^ 1111111111) << 11);
+  entropy ^= entropy << 11;
+  entropy += entropy << 5;
+  entropy ^= entropy >> 10;
   entropy += entropy << 3;
-  entropy ^= (entropy >> 10);
-  entropy += entropy << 3;
-  return ((entropy >> 1) << 10) ^ entropy;
+  return ((entropy << 10) ^ entropy) + 11;
 }
