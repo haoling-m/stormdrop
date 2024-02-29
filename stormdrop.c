8f30a4d2490ff554ab5a17ec7ca6b77316bcbecb
Wil Parsons <entrocraft@gmail.com>
2024-02-29 11:23:56 -0800

    Adding a multiplication by 9 to pass more BigCrush tests.

--- stormdrop.c
+++ stormdrop.c
@@ -3,6 +3,7 @@
 uint32_t stormdrop(uint32_t *state, uint32_t entropy) {
   entropy ^= entropy << 19;
   entropy ^= (*state ^ entropy) >> 3;
+  entropy += entropy << 3;
   (*state)++;
   entropy ^= (*state + entropy) << 6;
   entropy ^= entropy >> 7;
