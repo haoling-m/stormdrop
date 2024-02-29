d2c3310916b02c75e8a1d05e8bd598af710f3a8c
Wil Parsons <entrocraft@gmail.com>
2024-02-28 21:21:02 -0800

    Mixing in the state with the bit shuffling sequence.

--- stormdrop.c
+++ stormdrop.c
@@ -2,9 +2,8 @@
 
 void stormdrop(uint32_t *entropy) {
   entropy[1] ^= entropy[1] << 19;
-  entropy[1] ^= entropy[1] >> 3;
-  (entropy[0])++;
+  entropy[1] ^= (entropy[1] ^ (entropy[0])++) >> 3;
   entropy[1] ^= (entropy[0] + entropy[1]) << 6;
   entropy[1] ^= entropy[1] >> 7;
-  entropy[1] ^= (entropy[1] << 5) | 1;
+  entropy[1] ^= (entropy[1] << 5) ^ entropy[0];
 }
