39da1999bcfbb4788a0918ab55ea8bc04ba683e2
Wil Parsons <entrocraft@gmail.com>
2024-03-08 13:01:38 -0800

    Doubling the period with portable 32-bit state segments.

--- stormdrop.c
+++ stormdrop.c
@@ -1,13 +1,15 @@
 #include "stormdrop.h"
 
-static uint64_t state = 0;
+static uint32_t state[4] = {0, 0, 0, 0};
 
 uint32_t stormdrop(uint32_t entropy) {
-  entropy ^= entropy << 19;
-  entropy ^= (state ^ entropy) >> 3;
-  entropy += entropy << 3;
-  state++;
-  entropy ^= (state + entropy) << 6;
-  entropy ^= entropy >> 7;
-  return state ^ (entropy << 5);
+  entropy ^= entropy << 16;
+  state[0] ^= entropy;
+  entropy ^= (state[1] ^ entropy) >> 5;
+  (state[1])++;
+  state[2] ^= entropy;
+  entropy += entropy << 6;
+  state[3] ^= state[2] ^ entropy;
+  entropy ^= state[0] ^ (entropy >> 9);
+  return state[3] ^ entropy;
 }
