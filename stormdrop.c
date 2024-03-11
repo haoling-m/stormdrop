e081e984ba2a47679dc4b8a6967e6a1acd6b00a5
Wil Parsons <entrocraft@gmail.com>
2024-03-10 17:01:30 -0700

    Adding += in place of ^= to pass BigCrush MatrixRank tests.

--- stormdrop.c
+++ stormdrop.c
@@ -3,13 +3,13 @@
 static uint32_t state[4] = {0, 0, 0, 0};
 
 uint32_t stormdrop(uint32_t entropy) {
-  entropy ^= entropy << 16;
+  entropy += entropy << 16;
   state[0] ^= entropy;
   entropy ^= (state[1] ^ entropy) >> 5;
   (state[1])++;
   state[2] ^= entropy;
   entropy += entropy << 6;
-  state[3] ^= state[2] ^ entropy;
+  state[3] += state[2] ^ entropy;
   entropy ^= state[0] ^ (entropy >> 9);
   return state[3] ^ entropy;
 }
